6f7abbe3a433cf1084c43d91b37e99f6b0f71eb0
Luiz <nandoluizprimeiro@gmail.com>
2025-10-09 13:16:38 -0300

    Adicionando o index para corrigir rotas

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import NavBottom from "../components/navBottom";
 
 export default function Home() {
